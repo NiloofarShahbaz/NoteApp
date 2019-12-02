c869ba70e576b121e2d87582a0a9398893bebf39
Niloofar <niloofar1376@gmail.com>
2019-12-02 21:38:51 +0330

    Fix ListCreate and RetrieveUpdate api views

--- src/notes/serializers.py
+++ src/notes/serializers.py
@@ -4,8 +4,11 @@ from .models import Note
 
 
 class NoteSerializer(serializers.ModelSerializer):
+    owner = serializers.PrimaryKeyRelatedField(read_only=True)
+    collaborators = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    labels = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
     class Meta:
         model = Note
         exclude = ['trash_delete_time', ]
-
 
--- src/notes/views.py
+++ src/notes/views.py
@@ -1,52 +1,27 @@
-from django.shortcuts import render
+from django.db.models import Q
+
 from rest_framework import generics
 from rest_framework import permissions
-from rest_framework import status
-from rest_framework.response import Response
 
-from .models import Note
-from .serializers import NoteSerializer
+from .serializers import *
 
 
 class NoteListCreateView(generics.ListCreateAPIView):
-    permission_classes = (permissions.IsAuthenticated, )
+    permission_classes = (permissions.IsAuthenticated,)
     serializer_class = NoteSerializer
 
     def get_queryset(self):
-        return Note.objects.filter(archive=False, trash_delete_time=None, users=self.request.user)
+        return Note.objects.filter(Q(owner=self.request.user) | Q(collaborators__in=[self.request.user, ]),
+                                   is_archive=False, trash_delete_time=None)
 
-    def create(self, request, *args, **kwargs):
-        request.data['users'] = [self.request.user.id, ]
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class NoteRetrieveUpdateView(generics.RetrieveUpdateAPIView):
-    permission_classes = (permissions.IsAuthenticated, )
+    permission_classes = (permissions.IsAuthenticated,)
     serializer_class = NoteSerializer
 
     def get_queryset(self):
-        return Note.objects.filter(archive=False, trash_delete_time=None, users=self.request.user)
-
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        if not partial:
-            print('hello')
-            print(request.data['users'])
-            print(instance.get('users'))
-            request.data['users'] = request.data['users'].append(instance.users)
-
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-
-        if getattr(instance, '_prefetched_objects_cache', None):
-            # If 'prefetch_related' has been applied to a queryset, we need to
-            # forcibly invalidate the prefetch cache on the instance.
-            instance._prefetched_objects_cache = {}
-
-        return Response(serializer.data)
+        return Note.objects.filter(is_archive=False, trash_delete_time=None, owner=self.request.user,
+                                   pk=self.kwargs.get('pk'))
